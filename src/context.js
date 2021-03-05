e64f4ae9df1d7bbb9ab49b83df213d3b74a07850
Bhargav <Bhargavkumar909@gmail.com>
2021-03-05 13:57:22 +0530

    initial commit

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=537fe697`;
 
 const AppContext = React.createContext();
 
