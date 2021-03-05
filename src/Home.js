3d11d3e1757ee86b14769369db492355040fc7b5
Bhargav <Bhargavkumar909@gmail.com>
2021-03-05 11:35:52 +0530

    init fetch

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 import Form from './SearchForm'
 import Movies from './Movies'
 const Home = () => {
-  return <h2>movie component</h2>
+  return <main>
+    <Form />
+    <Movies />
+  </main>
 }
 
 export default Home
--- src/context.js
+++ src/context.js
@@ -1,15 +1,34 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from "react";
 
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
-console.log(API_ENDPOINT)
-const AppContext = React.createContext()
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("superman");
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    }catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
+  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+};
 
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
