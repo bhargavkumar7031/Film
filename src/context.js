3cd23fe7894360cd0eb534582a53a8ac62ce7658
Bhargav <Bhargavkumar909@gmail.com>
2021-03-05 12:56:12 +0530

    movie

--- src/context.js
+++ src/context.js
@@ -8,23 +8,33 @@ const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState({ show: false, msg: "" });
   const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState("superman");
+  const [query, setQuery] = useState("Marvel");
 
   const fetchMovies = async (url) => {
-    setIsLoading(true)
+    setIsLoading(true);
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
-    }catch(error) {
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchMovies(`${API_ENDPOINT}&s=${query}`);
   }, [query]);
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ isLoading, error, movies, setQuery, query }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
