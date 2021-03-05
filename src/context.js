edcf418e7225e024503931cc5ac4b9122dda5eba
Bhargav <Bhargavkumar909@gmail.com>
2021-03-05 11:16:14 +0530

    commit

--- src/context.js
+++ src/context.js
@@ -1,12 +1,13 @@
 import React, { useState, useContext, useEffect } from 'react'
-// make sure to use https
+
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+console.log(API_ENDPOINT)
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
